9234c57d15dc42933c1bd6f2012c4109fa87dda3
tonegi <90652377+tonegi@users.noreply.github.com>
2025-05-20 22:00:46 -0300

    Create Questao_03.js

---
+++ Questao_03.js
@@ -0,0 +1,29 @@
+let nome1 = "João da Silva";
+let nota1_1 = 7;
+let nota1_2 = 8.5;
+let peso1_1 = nota1_1 * 0.6;
+let peso1_2 = nota1_2 * 0.4;
+let media1 = peso1_1 + peso1_2;
+
+alert(`Aluno: ${nome1}\nNota 1: ${nota1_1}\nNota 2: ${nota1_2}`);
+alert(`Nota final de ${nome1}: ${media1.toFixed(2)}`);
+
+let nome2 = "Fulano";
+let nota2_1 = 9;
+let nota2_2 = 7.5;
+let peso2_1 = nota2_1 * 0.6;
+let peso2_2 = nota2_2 * 0.4;
+let media2 = peso2_1 + peso2_2;
+
+alert(`Aluno: ${nome2}\nNota 1: ${nota2_1}\nNota 2: ${nota2_2}`);
+alert(`Nota final de ${nome2}: ${media2.toFixed(2)}`);
+
+let nome3 = "Ciclano";
+let nota3_1 = 6.5;
+let nota3_2 = 9;
+let peso3_1 = nota3_1 * 0.6;
+let peso3_2 = nota3_2 * 0.4;
+let media3 = peso3_1 + peso3_2;
+
+alert(`Aluno: ${nome3}\nNota 1: ${nota3_1}\nNota 2: ${nota3_2}`);
+alert(`Nota final de ${nome3}: ${media3.toFixed(2)}`);
